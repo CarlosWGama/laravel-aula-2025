7e17ebf3d37e798b988454756055840f5f74cdc1
CarlosWGama <carloswgama@gmail.com>
2025-06-10 16:24:12 -0300

    Redirect login

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
 });
 
 //ROTAS DE AUTENTICAÇÃO
